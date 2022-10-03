d7070819434b0216bf4764a3ef9db1d8ff75e0e4
Gerry Kovan <gkovan@hotmail.com>
2022-10-03 19:49:35 -0400

    initial commit

---
+++ src/main/java/com/gk/aws/msk/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.gk.aws.msk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
