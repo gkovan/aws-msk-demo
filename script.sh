507d89f6f4de411754cca45d256b2a9000c39503
Gerry Kovan <gkovan@hotmail.com>
2023-01-12 14:51:01 -0500

    update

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ echo HELLO
 export BOOTSTRAP_SERVERS=mskdemonoauth.8f1ofw.c2.kafka.us-east-1.amazonaws.com:9092,b-1.mskdemonoauth.8f1ofw.c2.kafka.us-east-1.amazonaws.com:9092,b-2.mskdemonoauth.8f1ofw.c2.kafka.us-east-1.amazonaws.com:9092
 export PIPE_INPUT_TOPIC=streams-pipe-input
 export PIPE_OUTPUT_TOPIC=streams-pipe-output
-export APPLICATION_ID=-cloud9-streams-pipe
+export APPLICATION_ID=cloud9-streams-pipe
